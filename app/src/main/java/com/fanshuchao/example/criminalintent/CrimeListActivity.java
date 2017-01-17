72ec71e78ef4c3adc341f9507efc3cbfc10865e2
fanshuchao <fshuchao@126.com>
2017-01-17 13:51:38 +0800

    test RecyclerView

---
+++ app/src/main/java/com/fanshuchao/example/criminalintent/CrimeListActivity.java
@@ -0,0 +1,14 @@
+package com.fanshuchao.example.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by fanshuchao on 2017-1-17.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment(){
+        return new CrimeListFragment();
+    }
+}
