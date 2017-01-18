8fb17be8a97bb58bd7e495d174325886890909eb
fanshuchao <fshuchao@126.com>
2017-01-18 17:41:08 +0800

    test Intent

--- app/src/main/java/com/fanshuchao/example/criminalintent/CrimeBaseHelper.java
+++ app/src/main/java/com/fanshuchao/example/criminalintent/CrimeBaseHelper.java
@@ -23,7 +23,7 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
         db.execSQL("create table "+ CrimeDbSchema.CrimeTable.NAME + "(" +
         "_id integer primary key autoincrement,"+CrimeDbSchema.CrimeTable.Cols.UUID + "," +
         CrimeDbSchema.CrimeTable.Cols.TITLE + "," + CrimeDbSchema.CrimeTable.Cols.DATE + "," +
-        CrimeDbSchema.CrimeTable.Cols.SOLVED + ")"
+        CrimeDbSchema.CrimeTable.Cols.SOLVED + "," + CrimeDbSchema.CrimeTable.Cols.SUSPECT + ")"
         );
     }
 
